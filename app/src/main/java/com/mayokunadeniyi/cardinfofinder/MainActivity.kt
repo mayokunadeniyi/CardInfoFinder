325b3a7517e346024c2ce6e85ab8b244a2ea0a2f
GeorgCantor <cupsman1986@gmail.com>
2020-08-15 20:49:27 +0300

    cleaned up some code

--- app/src/main/java/com/mayokunadeniyi/cardinfofinder/MainActivity.kt
+++ app/src/main/java/com/mayokunadeniyi/cardinfofinder/MainActivity.kt
@@ -8,10 +8,12 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import com.mayokunadeniyi.cardinfofinder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setupNavigation()
     }
 
--- app/src/main/java/com/mayokunadeniyi/cardinfofinder/ui/inputfragment/InputFragment.kt
+++ app/src/main/java/com/mayokunadeniyi/cardinfofinder/ui/inputfragment/InputFragment.kt
@@ -10,12 +10,14 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.mayokunadeniyi.cardinfofinder.R
 import com.mayokunadeniyi.cardinfofinder.databinding.InputCardDetailsBinding
+import com.mayokunadeniyi.cardinfofinder.ui.inputfragment.InputFragmentDirections.Companion.actionInputFragmentToResultFragment
 
 /**
  * Created by Mayokun Adeniyi on 23/07/2020.
  */
 
-class InputFragment : Fragment(){
+class InputFragment : Fragment() {
+
     private lateinit var binding: InputCardDetailsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,20 +37,18 @@ class InputFragment : Fragment(){
     ): View? {
         binding = InputCardDetailsBinding.inflate(layoutInflater)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.inputFragSearchBtn.setOnClickListener {
-            if (binding.cardNumberEditText.text.isNullOrBlank()){
+            if (binding.cardNumberEditText.text.isNullOrBlank()) {
                 val shake = AnimationUtils.loadAnimation(requireContext(), R.anim.shake)
                 binding.cardNumberEditText.startAnimation(shake)
-            } else{
-                val text = binding.cardNumberEditText.text?.toString()
-                if (text != null){
-                    val cardNumber = text.toInt()
-                    val action = InputFragmentDirections.actionInputFragmentToResultFragment(cardNumber)
+            } else {
+                binding.cardNumberEditText.text?.toString()?.let {
+                    val cardNumber = it.toInt()
+                    val action = actionInputFragmentToResultFragment(cardNumber)
                     findNavController().navigate(action)
                 }
             }
